d014ed75037bf85b1eef4c5165851cb87734176d
CycloneJYC <463818057@qq.com>
2022-06-17 20:08:47 +0800

    完成了css打包

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+//引入js文件
 import {box1} from '../src/box1.js'
 import {box2} from '../src/box2.js'
-
 box1()
 box2()
+
+//引入css文件
+import './styles/index.css'
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,35 @@
-const path = require("path")
+// webpack默认能打包 js和json文件
+// 可以通过 loader 加载器打包css，less
 
+// css-loader
+// 1.yarn add css-loader style-loader
+// 2.配置 module.rules:[]
 
+// less-loader
+// 1.安装 yarn add less less-loader -D
+// 2.配置rules
+
+const path = require("path");
 
 module.exports = {
-    entry: "./src/index.js", 
-    output: { 
-        path: path.join(__dirname, "dist"), // 出口路径
-        filename: "main.js" // 出口文件名
-    },
-    mode: "development"
-}
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "dist"), // 出口路径
+    filename: "main.js", // 出口文件名
+  },
+  //mode 模式
+  mode: "development",
+
+  //   配置 loaders
+  module: {
+    rules: [
+      // css-loader
+      { 
+        test: /\.css/i, 
+        //从后往前解析，先通过css-loader解析，再通过style-loader解析
+        use: ["style-loader", "css-loader"] 
+      },
+      
+    ],
+  },
+};
